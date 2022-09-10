9f729d8b0af8c150523f72b5f4413027d6950bb1
Natan Junges Muller <natan.junges@hotmail.com>
2022-09-10 11:13:28 -0300

    refactor: test

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -8,6 +8,7 @@ defmodule Rockelivery do
   alias Rockelivery.Users.Get, as: UserGet
   alias Rockelivery.Users.Update, as: UserUpdate
 
+
   defdelegate create_user(params), to: UserCreate, as: :call
   defdelegate get_user_by_id(id), to: UserGet, as: :by_id
   defdelegate delete_user(id), to: UserDelete, as: :call
